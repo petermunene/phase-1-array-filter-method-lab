802d4d197113289de70c2f040c98c2881d7da3b6
petermunene <peter.munene@student.moringaschool.com>
2025-03-12 09:47:43 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+function fuzzyMatch(drivers,letter){
+    return drivers.filter(function(char){
+        return char.toLowerCase().startsWith(letter.toLowerCase())
+         })         
+}
+function matchName(drivers,name){
+    return drivers.filter(function(driver){
+        return driver.name===name
+    })
+
+}
+
